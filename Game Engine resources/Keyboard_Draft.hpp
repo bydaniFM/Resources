4d4d03f53b9fcd44974b41388e43040294085ad7
Daniel Fernández Marqués <daniFM333@gmail.com>
2017-11-21 16:42:31 +0100

    edited keyboard draft

--- Game Engine resources/Keyboard_Draft.hpp
+++ Game Engine resources/Keyboard_Draft.hpp
@@ -1,25 +1,25 @@
 
-#include <SDL.h>		// Crea dependencias
-
+#include <SDL.h>			// Crea dependencias por
+// estar en una cabecera
 class Keyboard
 {
 
-	enum Key			// Crea dependencias
+	enum Key
 	{
-		NUM_0 = SDLK_0,
+		NUM_0 = SDLK_0,		// Crea dependencias
 		NUM_1 = SDLK_1,
 		...
-		A 	  = SDLK_a,
+		A = SDLK_a,
 		...
-		DOWN  = SDLK_DOWN,
+		DOWN = SDLK_DOWN,
 		...
 	};
-	
+
 	struct Key
 	{
 		static const int NUM_0;
 	};
-	
+
 	enum Key
 	{
 		NUM_0,
@@ -31,25 +31,28 @@ class Keyboard
 		...
 	};
 
-	
+
 private:
-	
-	Key translate_key (int key_code);
-	
+
+	/// Transforma un código de tecla de SDL en un
+	/// código de tecla enumerado del engine.
+	Key translate_key(int key_code);
+
 };
 
 // EN KEYBOARD.CPP --------------------------------
 
 #include <SDL.h>		// No crea dependencias
+// al estar en un CPP
 
-Keyboard::Key Keyboard::translate_key (int key_code)
+Keyboard::Key Keyboard::translate_key(int key_code)
 {
 	switch (key_code)
 	{
-		case SDLK_0:    return NUM_0;
-		case SDLK_DOWN: return DOWN;
+	case SDLK_0:    return NUM_0;
+	case SDLK_DOWN: return DOWN;
 	}
-	
+
 	return UNDEFINED;
 }
 
